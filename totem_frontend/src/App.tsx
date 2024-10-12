ab42df58694a9d9556b525918cab98625582ecf9
Angelo Bongiorno <s331164@studenti.polito.it>
2024-10-12 19:23:37 +0200

    added SelectorComponent

--- totem_frontend/src/App.tsx
+++ totem_frontend/src/App.tsx
@@ -1,12 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TicketComponent from './TicketComponent'
+import TicketComponent from './components/TicketComponent'
+import SelectorComponent from './components/SelectorComponent'
+import API from './API'
 
 function App() {
 
-    const [count, setCount] = useState(0)
+  const [services, setServices] = useState<any>([]);
+  const [selectedService, setSelectedService] = useState<any>();
+  const [qrCode, setQrCode] = useState<string | undefined>();
+  const [ticketNumber, setTicketNumber] = useState<number | undefined>();
+  const [serviceId, setServiceId] = useState<number | undefined>();
+  const [queuePosition, setQueuePosition] = useState<number | undefined>();
+
+  // for now it only fetches available services once (component load time)
+  // might consider to update them when some condition is triggered
+  useEffect(() => {
+    const fetchServices = async () => {
+      try {
+        const response = await API.getServices();
+        setServices(response);
+      } catch (err) {
+        console.error('Error fetching services:', err);
+      }
+    };
+    fetchServices();
+  }, []);
+
+  useEffect(() => {
+    if (selectedService !== undefined) {
+      const fetchTicket = async () => {
+        try {
+          const response = await API.postTicket(selectedService.service_id);
+          setQrCode(response.qr);
+          setTicketNumber(response.ticket_code);
+          setServiceId(response.service_id);
+          setQueuePosition(response.queue_position);
+        } catch (error) {
+          console.error("error generating new ticket:", error);
+        } 
+      };
+      fetchTicket();
+    }
+  }, [selectedService]);
 
   return (
     <>
@@ -19,11 +57,9 @@ function App() {
         </a>
       </div>
       <h1>Totem Frontend</h1>
-      <TicketComponent />
+      {selectedService && <TicketComponent ticketNumber={ticketNumber} serviceId={serviceId} queuePosition={queuePosition} qrCode={qrCode}/>}
+      <SelectorComponent services={services} setSelectedService={setSelectedService}/>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- totem_frontend/src/components/TicketComponent.tsx
+++ totem_frontend/src/components/TicketComponent.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react';
-import API from './API'
-
 function TicketComponent(props: any) {
 
+  /*
   const [qrCode, setQrCode] = useState();
   const [ticketNumber, setTicketNumber] = useState();
   const [serviceId, setServiceId] = useState();
@@ -19,23 +17,17 @@ function TicketComponent(props: any) {
       console.error("error generating new ticket:", error);
     }
   };
+  */
 
 
   return(
     <div>
-      <h2>Get New Ticket</h2>
-      
-      {/*1 is passed for testing purposes, it should pass the required service id*/}
-      <button onClick={() => newTicket(1)}>Get Ticket</button>
-
-      {ticketNumber && (
         <div>
-          <p><strong>Ticket Number:</strong> {ticketNumber}</p>
-          <p><strong>Service ID:</strong> {serviceId}</p>
-          <p><strong>Queue Position:</strong> {queuePosition}</p>
-          <img src={qrCode} alt="QR Code" />
+          <p><strong>Ticket Number:</strong> {props.ticketNumber}</p>
+          <p><strong>Service ID:</strong> {props.serviceId}</p>
+          <p><strong>Queue Position:</strong> {props.queuePosition}</p>
+          <img src={props.qrCode} alt="QR Code" />
         </div>
-      )}
     </div>
   )
 }
