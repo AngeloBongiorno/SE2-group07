793e5a5f61c08a7d0d7746d5c62d463ed069b0f4
Angelo Bongiorno <s331164@studenti.polito.it>
2024-10-15 11:57:53 +0200

    POST nextCustomer

---
+++ officer_frontend/src/API.ts
@@ -0,0 +1,42 @@
+/*
+ * Complies to the POST nextCustomer API in API.md
+ * returns null and prints an error if something went wrong with request.
+ */
+
+
+const nextCustomer = async (counter_id: number): Promise<TicketToShow | null> => {
+    const requestUrl = `http://localhost:3001/officequeue/nextCustomer`;
+
+    try {
+      const response = await fetch(requestUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ counter_id }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok: " + response.statusText);
+      }
+      let calledTicket: TicketToShow = await response.json();
+      console.log('fetched this ticket: ', calledTicket);
+      return calledTicket;
+    } catch (error) {
+      console.error('Error fetching the next Customer ticket', error);
+      return null;
+    }
+}
+
+interface TicketToShow {
+    ticket_id: number,
+    service_type_id: number,
+    queue_position: number,
+    issued_at: string,
+    called_at: string,
+    status: string
+}
+
+
+
+const API = { nextCustomer };
+export default API;
