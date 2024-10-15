40477d02f07ff1b8d14fb1e545c614c5c64c0b9f
alinhi75 <100237261+alinhi75@users.noreply.github.com>
2024-10-15 22:23:58 +0200

    Revert "Ali"

--- officer_frontend/src/App.jsx
+++ officer_frontend/src/App.jsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Manager Frontend</h1>
+      <h1>Officer frontend</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
