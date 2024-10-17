580ac1b97e72026ab88b7f2424da7d28c1b39ce2
Lorenzo Gaetani s333048 <s333048@studenti.polito.it>
2024-10-17 15:22:54 +0200

    integeretion test

--- backend/src/test/integretionNextCustomer.test.ts
+++ backend/src/test/integretionNextCustomer.test.ts
@@ -8,6 +8,7 @@ const URL = "/officequeue";
 
 
 beforeAll(async () => {
+    jest.useFakeTimers();
     await cleanup();
     await asyncdb.asyncRun("INSERT INTO ServiceTypes (service_type_id, name, avg_service_time) VALUES (1,'account managing',10)")
     await asyncdb.asyncRun("INSERT INTO ServiceTypes (service_type_id, name, avg_service_time) VALUES (2,'product managing',20)")
@@ -23,9 +24,11 @@ beforeAll(async () => {
 })
 
 afterAll(async () => {
+    jest.runOnlyPendingTimers();
     await cleanup();
     await asyncdb.close();
 })
+
 
 describe("next customer with two queues", () => {
     test("wrong input string", async () => {
@@ -58,9 +61,45 @@ describe("next customer with two queues", () => {
 
         const response = await request(app).post(`${URL}/nextCustomer`).send({"counter_id": counter_id});
         expect(response.status).toBe(200);
-        console.log(response.body);
         expect(response.body.ticket_id).toBe(1);
         expect(response.body.status).toBe('called');
+    });
+    test("select the tickets that are waiting", async () => {
+        let counter_id = 1;
+        await cleanup();
+        await asyncdb.asyncRun("INSERT INTO ServiceTypes (service_type_id, name, avg_service_time) VALUES (1,'account managing',10)")
+        await asyncdb.asyncRun("INSERT INTO ServiceTypes (service_type_id, name, avg_service_time) VALUES (2,'product managing',20)")
+        await asyncdb.asyncRun("INSERT INTO Counters (counter_id, name) VALUES (1,'prova1')")
+        await asyncdb.asyncRun("INSERT INTO CounterServices (counter_service_id, counter_id, service_type_id) VALUES (1,1,1)")
+        await asyncdb.asyncRun("INSERT INTO CounterServices (counter_service_id, counter_id, service_type_id) VALUES (3,1,2)")
+        await asyncdb.asyncRun("INSERT INTO Tickets (ticket_id, service_type_id, queue_position, issued_at, called_at, status) VALUES(1,1,1,datetime('now'),NULL,'called');")
+        await asyncdb.asyncRun("INSERT INTO Tickets (ticket_id, service_type_id, queue_position, issued_at, called_at, status) VALUES(2,2,2,datetime('now'),NULL,'waiting');")
+
+        const response = await request(app).post(`${URL}/nextCustomer`).send({"counter_id": counter_id});
+        expect(response.status).toBe(200);
+        expect(response.body.ticket_id).toBe(2);
+        expect(response.body.status).toBe('called');
+    });
+    test("two counters work together", async () => {
+        let counter_id = 1;
+        await cleanup();
+        await asyncdb.asyncRun("INSERT INTO ServiceTypes (service_type_id, name, avg_service_time) VALUES (1,'account managing',10)")
+        await asyncdb.asyncRun("INSERT INTO ServiceTypes (service_type_id, name, avg_service_time) VALUES (2,'product managing',20)")
+        await asyncdb.asyncRun("INSERT INTO Counters (counter_id, name) VALUES (1,'prova1')")
+        await asyncdb.asyncRun("INSERT INTO Counters (counter_id, name) VALUES (2,'prova2')")
+        await asyncdb.asyncRun("INSERT INTO CounterServices (counter_service_id, counter_id, service_type_id) VALUES (1,1,1)")
+        await asyncdb.asyncRun("INSERT INTO CounterServices (counter_service_id, counter_id, service_type_id) VALUES (3,2,2)")
+        await asyncdb.asyncRun("INSERT INTO Tickets (ticket_id, service_type_id, queue_position, issued_at, called_at, status) VALUES(1,1,1,datetime('now'),NULL,'waiting');")
+        await asyncdb.asyncRun("INSERT INTO Tickets (ticket_id, service_type_id, queue_position, issued_at, called_at, status) VALUES(2,2,2,datetime('now'),NULL,'waiting');")
+
+        const response = await request(app).post(`${URL}/nextCustomer`).send({"counter_id": counter_id});
+        expect(response.status).toBe(200);
+        expect(response.body.ticket_id).toBe(1);
+        expect(response.body.status).toBe('called');
+        const response2 = await request(app).post(`${URL}/nextCustomer`).send({"counter_id": 2});
+        expect(response2.status).toBe(200);
+        expect(response2.body.ticket_id).toBe(2);
+        expect(response2.body.status).toBe('called');
     });
     test("no tickets has to be served", async () => {
         let counter_id = 1;
@@ -70,6 +109,20 @@ describe("next customer with two queues", () => {
         await asyncdb.asyncRun("INSERT INTO Counters (counter_id, name) VALUES (1,'prova1')")
         await asyncdb.asyncRun("INSERT INTO CounterServices (counter_service_id, counter_id, service_type_id) VALUES (1,1,1)")
         await asyncdb.asyncRun("INSERT INTO Tickets (ticket_id, service_type_id, queue_position, issued_at, called_at, status) VALUES(1,2,1,datetime('now'),NULL,'waiting');")
+        await asyncdb.asyncRun("INSERT INTO Tickets (ticket_id, service_type_id, queue_position, issued_at, called_at, status) VALUES(2,2,2,datetime('now'),NULL,'waiting');")
+
+        const response = await request(app).post(`${URL}/nextCustomer`).send({"counter_id": counter_id});
+        expect(response.status).toBe(200);
+        expect(response.body).toStrictEqual({});
+    });
+    test("no tickets has to be served for my services", async () => {
+        let counter_id = 1;
+        await cleanup();
+        await asyncdb.asyncRun("INSERT INTO ServiceTypes (service_type_id, name, avg_service_time) VALUES (1,'account managing',10)")
+        await asyncdb.asyncRun("INSERT INTO ServiceTypes (service_type_id, name, avg_service_time) VALUES (2,'product managing',20)")
+        await asyncdb.asyncRun("INSERT INTO Counters (counter_id, name) VALUES (1,'prova1')")
+        await asyncdb.asyncRun("INSERT INTO CounterServices (counter_service_id, counter_id, service_type_id) VALUES (1,1,1)")
+        await asyncdb.asyncRun("INSERT INTO Tickets (ticket_id, service_type_id, queue_position, issued_at, called_at, status) VALUES(1,2,2,datetime('now'),NULL,'waiting');")
         await asyncdb.asyncRun("INSERT INTO Tickets (ticket_id, service_type_id, queue_position, issued_at, called_at, status) VALUES(2,2,2,datetime('now'),NULL,'waiting');")
 
         const response = await request(app).post(`${URL}/nextCustomer`).send({"counter_id": counter_id});
